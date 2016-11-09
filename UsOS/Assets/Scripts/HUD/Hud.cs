fe22992c2720d4209142185e4fa0db8360b56be4
Aleksandar Penev <aleksandar_penev@abv.bg>
2016-11-09 21:28:20 +0200

    Import Package

---
+++ UsOS/Assets/Scripts/HUD/Hud.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    [SerializeField]
+    private Text score;
+    internal static Hud instance;
+
+    public void Awake()
+    {
+        instance = this.GetComponent<Hud>();
+    }
+
+    public void UpdateScoreLabel()
+    {
+        //this.score.text = "Laser hits: " + GameData.hitCounter;
+    }
+}
---
+++ UsOS/Assets/Scripts/LaserCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+class LaserCollider : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Korey")
+        {
+            //GameData.hitCounter++;
+            Hud.instance.UpdateScoreLabel();
+        }
+    }
+}
